7eb4cff2cc9c0b5f2699cc39338bf7dcccf186ce
davidjrb <davidjberman@gmail.com>
2018-01-24 07:22:24 +0000

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -3,7 +3,7 @@ var myVar = setInterval(myTimer ,1000);
 function myTimer() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
+    {
       document.getElementById("demo").innerHTML = this.responseText;
     }
   };
